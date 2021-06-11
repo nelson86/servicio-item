6f9bcbb31c3b9d538ed29b6371251ac3ae348869
arabelne <nelson.arabel@lamercantil.com.ar>
2021-06-11 08:52:00 -0300

    Proyecto base

---
+++ src/main/java/com/narabel/servicioitem/ServicioItemApplication.java
@@ -0,0 +1,13 @@
+package com.narabel.servicioitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioItemApplication.class, args);
+	}
+
+}
